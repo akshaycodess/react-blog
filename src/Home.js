c36787dc1b573257cd3f768e02b10348cdd2564a
Akshay Singh <jobakshay21@gmail.com>
2024-06-14 13:14:59 +0530

    conditional loading message with setTimeout

--- src/Home.js
+++ src/Home.js
@@ -3,22 +3,27 @@ import Bloglist from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setisPending] = useState(true);
 
     // const [name, setName] = useState('mario');
 
     // runs on every render, can be used to fetch datas
     useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
-            setBlogs(data);
-        });
+        setTimeout(() => {
+          fetch("http://localhost:8000/blogs")
+            .then((res) => {
+              return res.json();
+            })
+            .then((data) => {
+              setBlogs(data);
+              setisPending(false);
+            });
+        }, 1000);
     }, []);
 
     return (
     <div className="home">
+        {isPending && <div>loading...</div>}
         {blogs && (<Bloglist blogs={blogs} title="All Blogs!" />)}
         {/* <Bloglist blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!" /> */}
         {/* <button onClick={() => setName('luigi')}>change name</button>
