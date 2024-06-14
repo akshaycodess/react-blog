d25ee272bf28245c51e5634a18cb5d004d3f5a00
Akshay Singh <jobakshay21@gmail.com>
2024-06-14 15:20:52 +0530

    created separate fetch component to increase reusability

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,10 @@
 import { useState, useEffect } from "react";
 import Bloglist from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setisPending] = useState(true);
-    const [error, setError] = useState(null);
-
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
     // const [name, setName] = useState('mario');
-
-    // runs on every render, can be used to fetch datas
-    useEffect(() => {
-        setTimeout(() => {
-          fetch("http://localhost:8000/blogs")
-            .then(res => {
-              if(!res.ok) {
-                throw Error('could not fetch the data for that resource');
-              }
-              return res.json();
-            })
-            .then(data => {
-              setBlogs(data);
-              setisPending(false);
-              setError(null);
-            })
-            .catch(err => {
-              setError(err.message);
-              setisPending(false);
-            });
-        }, 1000);
-    }, []);
 
     return (
     <div className="home">
