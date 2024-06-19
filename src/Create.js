825530df639427d97bb0dcf2a30ef641479fdab6
Akshay Singh <jobakshay21@gmail.com>
2024-06-19 13:36:14 +0530

    submit handler

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,17 @@ const Create = () => {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
     const [author, setAuthor] = useState("mario");
+    
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const blogDetails = { title, body, author };
+      console.log(blogDetails);
+    }
 
     return (
       <div className="create">
         <h2>Add a New Blog</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Blog title:</label>
           <input
             type="text"
